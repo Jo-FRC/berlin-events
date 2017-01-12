d337e41993ab56cff656011dcc35890209e6b24a
Shirin Tag <taghavi.shirin@gmail.com>
2017-01-12 11:08:25 +0100

    first step of styling and css

--- public/client.js
+++ public/client.js
@@ -1,58 +1,5 @@
 var myApp = angular.module('myApp', ['ui.router', 'ngCookies'])
 
-.service('LoggedInServ', function($http, $state, $cookies){
-    var LoggedInService = this;
-    var loggedIn = $cookies.get('isLoggedIn');
-    this.submitLogin = function(username, password){
-        var url = 'berlinevents/login';
-        var userData = { username, password };
-        var jsonData = JSON.stringify(userData);
-        return $http.post(url, jsonData)
-        .then(function(result){
-            LoggedInService.username = result.data.username;
-            console.log(LoggedInService);
-            console.log(result.data.username);
-            // console.log(loggedIn);
-            // console.log(result.data);
-            console.log('this is successful');
-            $cookies.put('isLoggedIn', true);
-            loggedIn = true;
-            $cookies.put('username', LoggedInService.username);
-            // $state.go('home');
-        })
-        .catch(function(err){
-            console.log(err);
-        });
-    };
-    this.checkIfLoggedIn = function(){
-        return loggedIn;
-
-    };
-    this.logout = function(){
-        loggedIn = false;
-        $cookies.remove('isLoggedIn');
-        delete LoggedInService.username;
-    };
-    this.registerUser = function(){
-        loggedIn = true;
-        $cookies.put('isLoggedIn', true);
-    };
-    this.getUsername = function(){
-        return new Promise(function(resolve, reject){
-            if (LoggedInService.username) {
-                resolve(LoggedInService.username);
-            } else {
-                $http.get('/getUserinfo').then(function(result){
-                    LoggedInService.username = result.data.username;
-                    resolve(result.data.username);
-                });
-            }
-
-        });
-
-    };
-})
-
 .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
     $urlRouterProvider.otherwise('/berlinevents');
     $stateProvider
@@ -135,107 +82,134 @@ var myApp = angular.module('myApp', ['ui.router', 'ngCookies'])
         }
 
     })
-.state('logout', {
-    url : '/logout',
-    controller: function($scope, $http, $state, LoggedInServ, $cookies){
-        LoggedInServ.logout();
-        delete $scope.signedUp;
-        delete $scope.loggedIn;
-        delete $scope.linkUpload;
-        $state.go('home');
-    }
-})
-.state('addlink', {
-    url : '/addlink',
-    templateUrl: '/addlink.html',
-    controller: function($scope, $http, $state, LoggedInServ, $cookies){
-        $scope.isLoggedIn = LoggedInServ.checkIfLoggedIn();
-        if ($scope.isLoggedIn) {
-            $scope.recentLinks = [];
-            $scope.link = {};
-            var url = '/berlinevents';
-            $http.get(url).then(function(result){
-                console.log(result.data.links);
+    .state('logout', {
+        url : '/logout',
+        controller: function($scope, $http, $state, LoggedInServ, $cookies){
+            LoggedInServ.logout();
+            delete $scope.signedUp;
+            delete $scope.loggedIn;
+            delete $scope.linkUpload;
+            $state.go('home');
+        }
+    })
+    .state('addlink', {
+        url : '/addlink',
+        templateUrl: '/addlink.html',
+        controller: function($scope, $http, $state, LoggedInServ, $cookies){
+            $scope.isLoggedIn = LoggedInServ.checkIfLoggedIn();
+            if ($scope.isLoggedIn) {
+                $scope.recentLinks = [];
+                $scope.link = {};
+                var url = '/berlinevents';
+                $http.get(url).then(function(result){
+                    console.log(result.data.links);
 
-                $scope.recentLinks = result.data.links;
-            });
-            $scope.upload = function(){
-
-                var url = '/berlinevents/link';
-                var title = $scope.link.title;
-                var link = $scope.link.url;
-                console.log(link);
-                if (link.substring(0, 4) !== 'http') {
-                    link = 'http://' + link;
-                }
-                var recent =  $scope.recentLinks;
-                console.log(recent);
-                var matching = recent.some(function(linkToCompare){
-                    console.log(link);
-                    console.log(linkToCompare.link);
-                    return linkToCompare.link == link;
+                    $scope.recentLinks = result.data.links;
                 });
+                $scope.upload = function(){
 
-                if (matching) {
-                    alert('This link already exists');
-                    delete $scope.linkUpload;
-                    return;
-                }
+                    var url = '/berlinevents/link';
+                    var title = $scope.link.title;
+                    var link = $scope.link.url;
+                    console.log(link);
+                    if (link.substring(0, 4) !== 'http') {
+                        link = 'http://' + link;
+                    }
+                    var recent =  $scope.recentLinks;
+                    console.log(recent);
+                    var matching = recent.some(function(linkToCompare){
+                        console.log(link);
+                        console.log(linkToCompare.link);
+                        return linkToCompare.link == link;
+                    });
 
-                var data = {
-                    title : title,
-                    url : link
+                    if (matching) {
+                        alert('This link already exists');
+                        delete $scope.linkUpload;
+                        return;
+                    }
+
+                    var data = {
+                        title : title,
+                        url : link
+                    };
+
+                    $http.post(url, data).then(function(result){
+                        $scope.success = true;
+                        $scope.recentLinks.unshift(result.data);
+                        $state.go('home');
+                    });
+
                 };
 
-                $http.post(url, data).then(function(result){
-                    $scope.success = true;
-                    $scope.recentLinks.unshift(result.data);
-                    $state.go('home');
+            } else {
+                console.log('test');
+                $state.go('login', {
+                    next: 'addlink'
                 });
-
-            };
-
-        } else {
-            console.log('test');
-            $state.go('login', {
-                next: 'addlink'
-            });
+            }
         }
-    }
-})
-.state('comments', {
-    url : '/comment/:id',
-    templateUrl: '/comments.html',
-    controller: function($scope, $http, $state, LoggedInServ, $cookies, $stateParams){
-        $scope.isLoggedIn = LoggedInServ.checkIfLoggedIn();
-        if ($scope.isLoggedIn) {
-            LoggedInServ.getUsername().then(function(name){
-                $scope.username = name;
-            });
-            $scope.recentComments = [];
-            $scope.comment = {};
-            var url = '/link/' + $stateParams.id;
-
-            $http.get(url).then(function(result){
-                console.log(result.data.comments);
-                $scope.recentComments = result.data.comments;
-            });
-            $scope.addComment = function(){
+    })
+    .state('comments', {
+        url : '/comment/:id',
+        templateUrl: '/comments.html',
+        controller: function($scope, $http, $state, LoggedInServ, $cookies, $stateParams){
+            $scope.isLoggedIn = LoggedInServ.checkIfLoggedIn();
+            if ($scope.isLoggedIn) {
+                LoggedInServ.getUsername().then(function(name){
+                    $scope.username = name;
+                });
+                $scope.recentComments = [];
+                $scope.comment = {};
                 var url = '/link/' + $stateParams.id;
-                var comment = $scope.comment;
-                console.log(comment);
-                $http.post(url, comment).then(function(result){
-                    console.log(result);
-                    $scope.recentComments.unshift(result.data);
-                    delete $scope.comment.text;
+
+                $http.get(url).then(function(result){
+                    console.log(result.data.comments);
+                    $scope.recentComments = result.data.comments;
                 });
-            };
-        } else {
-            $state.go('login', {
-                next: 'comments',
-                id: $stateParams.id
-            });
+
+                $scope.addComment = function(){
+                    var url = '/link/' + $stateParams.id;
+                    var comment = $scope.comment;
+                    console.log(comment);
+                    $http.post(url, comment).then(function(result){
+                        console.log(result);
+                        $scope.recentComments.unshift(result.data);
+                        delete $scope.comment.text;
+                    });
+                };
+                $scope.respond = function(){
+                    var url = '/link/' + $stateParams.id;
+                    var comment = $scope.comment;
+                    console.log(comment);
+                    $http.post(url, comment).then(function(result){
+                        console.log(result);
+                        $scope.responded = true;
+                        $scope.recentComments.unshift(result.data);
+                        delete $scope.comment.text;
+                    });
+                };
+            } else {
+                $state.go('login', {
+                    next: 'comments',
+                    id: $stateParams.id
+                });
+            }
         }
-    }
-});
+    })
+    .state('profile', {
+        url : '/profile',
+        templateUrl: '/profile.html',
+        controller: function($scope, $http, $state, LoggedInServ, $cookies){
+            $scope.isLoggedIn = LoggedInServ.checkIfLoggedIn();
+            if ($scope.isLoggedIn) {
+                LoggedInServ.getUsername().then(function(name){
+                    $scope.username = name;
+                });
+                $scope.profile = function(){
+
+                };
+            }
+        }
+    });
 }]);
---
+++ public/services/service.LoggedInServ.js
@@ -0,0 +1,55 @@
+
+angular.module('app.service.LoggedInServ', ['ui.router', 'ngCookies'])
+
+myApp.service('LoggedInServ', function($http, $state, $cookies){
+    var LoggedInService = this;
+    var loggedIn = $cookies.get('isLoggedIn');
+    this.submitLogin = function(username, password){
+        var url = 'berlinevents/login';
+        var userData = { username, password };
+        var jsonData = JSON.stringify(userData);
+        return $http.post(url, jsonData)
+        .then(function(result){
+            LoggedInService.username = result.data.username;
+            console.log(LoggedInService);
+            console.log(result.data.username);
+            // console.log(loggedIn);
+            // console.log(result.data);
+            console.log('this is successful');
+            $cookies.put('isLoggedIn', true);
+            loggedIn = true;
+            $cookies.put('username', LoggedInService.username);
+            // $state.go('home');
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    };
+    this.checkIfLoggedIn = function(){
+        return loggedIn;
+
+    };
+    this.logout = function(){
+        loggedIn = false;
+        $cookies.remove('isLoggedIn');
+        delete LoggedInService.username;
+    };
+    this.registerUser = function(){
+        loggedIn = true;
+        $cookies.put('isLoggedIn', true);
+    };
+    this.getUsername = function(){
+        return new Promise(function(resolve, reject){
+            if (LoggedInService.username) {
+                resolve(LoggedInService.username);
+            } else {
+                $http.get('/getUserinfo').then(function(result){
+                    LoggedInService.username = result.data.username;
+                    resolve(result.data.username);
+                });
+            }
+
+        });
+
+    };
+})
